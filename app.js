6ab3e347c0d7c43122283ddc1c41494a10a16bdb
Ezequiel <ezemazzucco@gmail.com>
2023-11-21 01:41:10 -0300

    form

--- app.js
+++ app.js
@@ -7,10 +7,23 @@ const publicPath = path.resolve(__dirname, './public')
 
 app.use( express.static(publicPath));
 
-app.listen(3000, ()=>{
-    console.log("Servidor corriendo en mi puerto 3000")
-})
+// app.listen(3000, ()=>{
+//     console.log("Servidor corriendo en mi puerto 3000")
+// })
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Servidor corriendo en el puerto 3000");
+});
 
 app.get('/',(req, res)=>{
     res.sendFile(path.resolve(__dirname,'./views/home.html'));
 })
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname,"./views/register.html"));
+})
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname,"./views/login.html"));
+})
+
